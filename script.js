93f6ad587567039d4d2c108803f90862a929c2e2
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-07-04 10:06:12 -0400

    Navbar creation

---
+++ script.js
@@ -0,0 +1,5 @@
+const navbarTogglerButton = document.getElementById('navbarTogglerButton');
+navbarTogglerButton.addEventListener('click', function(){
+    const navbarTogglerDisplay = document.getElementById('navbarTogglerDisplay');
+    navbarTogglerDisplay.classList.toggle('show');
+})
